cf17c492091e0b9376d76dc6a87bba13f8054d5c
rakrakon <68773850+rakrakon@users.noreply.github.com>
2025-06-02 14:17:57 +0300

    Add color constants

--- app/app/src/main/java/com/rakra/wordsprint/MainActivity.kt
+++ app/app/src/main/java/com/rakra/wordsprint/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.rakra.wordsprint
 
 import android.os.Bundle
+import android.view.Window
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -8,13 +9,17 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.core.view.WindowCompat
 import com.rakra.wordsprint.ui.theme.WordSprintTheme
 
-val ALEF_FONT = FontFamily(
-    Font(R.font.alef_regular, FontWeight.Normal)
+val BACKGROUND_COLOR = Color(0xFF3D2D46)
+
+val RUBIK_FONT = FontFamily(
+    Font(R.font.rubik_regular, FontWeight.Normal)
 )
 
 enum class Screen {
@@ -25,6 +30,7 @@ enum class Screen {
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         enableEdgeToEdge()
         setContent {
             WordSprintTheme {

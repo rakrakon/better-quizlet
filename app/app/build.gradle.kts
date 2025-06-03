0235f0664b87f6fea9acc50c18c1b24b2386e156
rakrakon <68773850+rakrakon@users.noreply.github.com>
2025-06-04 00:46:08 +0300

    Add viewmodel

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -59,6 +59,8 @@ dependencies {
 
     implementation(libs.kotlinx.serialization.json)
 
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
